a6bc06814b8d7fd018d17a039b44f725bf6fca4d
mangming2 <59263564+mangming2@users.noreply.github.com>
2021-09-18 19:22:05 +0900

    [test]: scss 적용 테스트2

--- frontend/army_mentoring/src/components/Header/Header.js
+++ frontend/army_mentoring/src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React,{ Component } from "react";
 import logo from '../logo.png';
 import './Header.scss';
+
+
 class Header extends Component{
     render(){
-        return(
-           
-            
+        return(      
+           <> 
     <Header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="header1">헤더입니다</h1>
     </Header>
-           
+          </> 
         )
     }
 }
