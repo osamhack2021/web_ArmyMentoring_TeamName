9b937123a1bc9c0d1e9c44e8529fe803fe6e5750
mangming2 <59263564+mangming2@users.noreply.github.com>
2021-09-18 22:48:21 +0900

    sass delete

--- frontend/army_mentoring/src/components/Header/Header.js
+++ frontend/army_mentoring/src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React,{ Component } from "react";
-import logo from '../logo.png';
-import './Header.scss';
+import './Header.css';
+
+
+
+
 
 
 class Header extends Component{
     render(){
-        return(      
-           <> 
-    <Header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="header1">헤더입니다</h1>
-    </Header>
-          </> 
+        return(
+            
+            <h2 className='header2'>This is Header</h2>
+            
+            
         )
     }
 }
