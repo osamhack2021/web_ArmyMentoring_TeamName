3bca08aa793a45ae6ea68326ee4bae04b7cc64c2
nagi0101 <62392317+nagi0101@users.noreply.github.com>
2021-09-24 13:50:52 +0000

    [etc]: 코드 린팅 작업
    <description>
    pylint 규칙에 따라 코드 린팅 작업함.
    불필요한 세미콜론 삭제, 파일 마지막 줄에 공백 추가 등.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
 
 class AbstractTimeStampModel(models.Model):
-    created_at=models.DateTimeField(auto_now=True);
-    updated_at=models.DateTimeField(auto_now_add=True);
+    created_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        abstract=True;
+        abstract=True
+        
