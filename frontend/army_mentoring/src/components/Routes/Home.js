ca7e9aa1d6d5e02a1037a7d84ce755114210bbfc
mangming2 <leejiho35@gmail.com>
2021-09-24 22:34:13 +0900

    home

--- frontend/army_mentoring/src/components/Routes/Home.js
+++ frontend/army_mentoring/src/components/Routes/Home.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React,{useState} from "react";
+import './Home.scss';
+
+
 
 function Home(){
-   
+
+    
+    const toggling=(e)=>{
+    const clicked= e.target.parentElement;
+    const toggled= clicked.nextSibling;
+    if(toggled.classList.contains("hide")){
+        toggled.classList.remove("hide");
+    } else {
+        toggled.classList.add("hide");
+      }
+    }
         return(
-            
-            <h2>This is Home</h2>
-           
+            <div>
+                <h2>This is Home</h2>
+
+                <div className='service home'>
+                    <h2>군대에서의 멘토링 서비스</h2>
+                </div>
+
+                <div className='mentoring home'>
+                    <h2>멘토링 가능한 목록들</h2>
+                </div>  
+
+                <div className='howtodo home'> 
+                    <h2>진행과정</h2>
+                </div> 
+
+                <div className='review home'> 
+                    <h2>멘토링 후기</h2>
+                </div> 
+
+                <div className='faqs home'> 
+                    <h2>FAQs</h2>
+                    <div className='QnA'>
+                        
+                        <div className='Q'>
+                            <p>질문</p>
+                            <button onClick={toggling}/>
+                        </div>
+
+                        <div className='A'>
+                            <p>
+                                    대답
+                            </p>
+                        </div>
+                        
+
+                    </div>
+
+                    <div className='QnA'>
+                        
+                        <div className='Q'>
+                            <p>질문2</p>
+                            <button onClick={toggling}/>
+                        </div>
+
+                        <div className='A'>
+                            <p>
+                                    대답2
+                            </p>
+                        </div>
+                        
+
+                    </div>
+                </div> 
+            </div>
         )
     
 }
