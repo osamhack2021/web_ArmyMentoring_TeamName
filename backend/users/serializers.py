786bd656ee3dad83faa6673e5de159071c021fb7
Yanghyeondong <37038105+Yanghyeondong@users.noreply.github.com>
2021-09-25 06:58:14 +0000

    [feature]: serializers 추가 (users,tags,questions)
    users,tags,questions 모델을 위한 serializers 추가.
    형식은 portfolio 앱 참조.

---
+++ backend/users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from users.models import User, UserReview
+
+class UserReviewSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = UserReview
+        fields = ['mentor', 'mentee', 'content']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    user_reviews = UserReviewSerializer()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'nickname', 'profile_image', 'experience_point', 'description', 'user_reviews']
