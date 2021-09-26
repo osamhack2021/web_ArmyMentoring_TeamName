c68ca6e11ed37a2b383c2015f115ecdffb14bdce
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-09-26 06:00:21 +0000

    [feature, update] : #003 feature 생성 및 개발 <description> #003 이슈의 페이지 서브 메뉴바 feature를 생성하고 기본 내용과 스타일 지정, 바로가기 기능 추가

--- frontend/army_mentoring/src/components/Routes/Mentoring.js
+++ frontend/army_mentoring/src/components/Routes/Mentoring.js
@@ -5,18 +5,24 @@ import './Mentoring.scss';
 function Mentoring(){
 
     return (
-        <div>
-            <h2>this is mentoring</h2>
-            
-            <div className="section">
+        <div>            
+            <div className="subnavbar">
+                <ul>
+                    <li><a href="#sgstmentoring">추천 멘토링</a></li>
+                    <li><a href="#sgstmentor">추천 멘토</a></li>
+                    <li><a href="#srchmentoring">멘토링 검색</a></li>
+                </ul>
+            </div>
+
+            <div className="section" id="sgstmentoring">
                 <h2>추천 멘토링</h2>
             </div>
 
-            <div className="section">
+            <div className="section" id="sgstmentor">
                 <h2>추천 멘토</h2>
             </div>
 
-            <div className="section">
+            <div className="section" id="srchmentoring">
                 <h2>멘토링 검색</h2>
             </div>
         </div>
