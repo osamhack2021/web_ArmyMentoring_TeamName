feff55170d7c88736c4096ddb9161b2982acd1e4
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-09-26 14:11:27 +0000

    [update] : #003 feature 개발 <description> #003 이슈의 페이지 서브 메뉴바의 화면 고정 기능 추가

--- frontend/army_mentoring/src/components/Routes/Mentoring.js
+++ frontend/army_mentoring/src/components/Routes/Mentoring.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Mentoring.scss';
 
 function Mentoring(){
 
+    const setNavPos = (e)=>{
+        const u = document.getElementById('test');
+        if(window.pageYOffset > 80)
+            u.className = 'fixedNav';
+        else
+            u.className = 'staticNav';
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', setNavPos);
+        return ()=>{
+            window.removeEventListener('scroll', setNavPos);
+        }
+    });
+
     return (
         <div>            
             <div className="subnavbar">
-                <ul>
+                <ul id="test">
                     <li><a href="#sgstmentoring">추천 멘토링</a></li>
                     <li><a href="#sgstmentor">추천 멘토</a></li>
                     <li><a href="#srchmentoring">멘토링 검색</a></li>
