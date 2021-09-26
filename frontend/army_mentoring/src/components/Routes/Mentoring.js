5fa12fba73759d2da4184c6994696ac6d671778a
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-09-26 14:25:19 +0000

    [update] : #003 feature 개발 #003 이슈의 페이지 서브 메뉴바의 하이라이팅 기능 추가

--- frontend/army_mentoring/src/components/Routes/Mentoring.js
+++ frontend/army_mentoring/src/components/Routes/Mentoring.js
@@ -4,27 +4,46 @@ import './Mentoring.scss';
 
 function Mentoring(){
 
-    const setNavPos = (e)=>{
+    /* sub nav bar 화면 따라가기 */
+    const setNavStyle = (e)=>{
         const u = document.getElementById('test');
         if(window.pageYOffset > 80)
             u.className = 'fixedNav';
         else
             u.className = 'staticNav';
     }
+
     useEffect(()=>{
-        window.addEventListener('scroll', setNavPos);
+        window.addEventListener('scroll', setNavStyle);
         return ()=>{
-            window.removeEventListener('scroll', setNavPos);
+            window.removeEventListener('scroll', setNavStyle);
         }
     });
+
+    /* sub nav bar 하이라이팅 */
+    const highlighting = (e)=>{
+        const clicked = e.target.parentNode;
+        clicked.className = "clicked";
+        
+        var sib = clicked.nextSibling;
+        while(sib){
+            sib.className = "";
+            sib = sib.nextSibling;
+        }
+        var sib = clicked.previousSibling;
+        while(sib){
+            sib.className = "";
+            sib = sib.previousSibling;
+        }
+    }
 
     return (
         <div>            
             <div className="subnavbar">
                 <ul id="test">
-                    <li><a href="#sgstmentoring">추천 멘토링</a></li>
-                    <li><a href="#sgstmentor">추천 멘토</a></li>
-                    <li><a href="#srchmentoring">멘토링 검색</a></li>
+                    <li onClick={highlighting}><a href="#sgstmentoring">추천 멘토링</a></li>
+                    <li onClick={highlighting}><a href="#sgstmentor">추천 멘토</a></li>
+                    <li onClick={highlighting}><a href="#srchmentoring">멘토링 검색</a></li>
                 </ul>
             </div>
 
