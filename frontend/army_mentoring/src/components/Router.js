57a1463c25dc5ad313f6a7184a3ed6116a84efa2
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-10-02 14:49:50 +0000

    [feature, update] : close #025, login/signup/mypage 기초 구성, axios 추가 axios를 yarn.lock에 추가, login/signup/mypage 기초 요소 구성, axios를 이용한 간단한 로그인 기능 구현(백엔드 서버 연결 없이)

--- frontend/army_mentoring/src/components/Router.js
+++ frontend/army_mentoring/src/components/Router.js
@@ -10,7 +10,7 @@ import Mentoringspecificmento from "./Routes/Mentoring/Mentoringspecificmento";
 import Mentoringspecificmentee from "./Routes/Mentoring/Mentoringspecificmentee";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MentorIntroduction from "./Routes/Mentoring/Mentorintroduction";
-
+import Login from './Routes/Login';
 
 export default () => (
   
@@ -22,7 +22,7 @@ export default () => (
       <Route path="/mymentoringlist" component={Mymentoringlist}></Route>
       <Route path="/community" component={community}></Route>
       <Route path="/qna" component={qna}></Route>
-      <Route path="/mypage" component={mypage}></Route>
+      <Route path="/mypage" component={Login}></Route>
       <Route path="/mentorintro" component={MentorIntroduction}></Route>
       <Route path="/mentoringintro" component={Mentoringintroduction}></Route>
       <Route path="/makementoring" component={Makementoring}></Route>
