a6a878f7a14e7741097bc574c97d1dcf53d25d15
Yanghyeondong <37038105+Yanghyeondong@users.noreply.github.com>
2021-10-03 14:17:25 +0000

    [update]: Permission 설정
    기본 settings.py 의 값은 AllowAny 로,
    나머지 각 앱의 경우 IsAuthenticatedOrReadOnly 로 수정
    28

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,8 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        #'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+        #'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,11 +2,14 @@ from rest_framework import viewsets
 
 from users.models import User, UserReview
 from users.serializers import UserSerializer, UserReviewSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset=User.objects.all()
     serializer_class=UserSerializer
 
 class UserReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset= UserReview.objects.all()
     serializer_class=UserReviewSerializer
