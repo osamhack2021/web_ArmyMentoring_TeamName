13e80e12b396b8cf7f8b513d262651618b00bea7
nagi0101 <62392317+nagi0101@users.noreply.github.com>
2021-10-03 05:30:38 +0000

    [feature]: Chatting 서버 개발을 위한 기본적인 준비 완료.
    <description>
    Socket.IO를 이용한 채팅 서버 개발을 위한 기본적인 세팅과
    socket 통신 테스트를 마쳤다.
    
    <issue-number>
    Close #20

---
+++ chatting/src/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+
+const app = express();
+const PORT = process.env.port || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+const server = createServer(app);
+const io = new Server(server, {});
+
+io.on('connection', (socket)=>{
+    socket.on('joinRoom', (roomName, user)=>{
+        console.log(`${socket.id} joinRoom`);
+        socket.join(roomName, () => {
+            console.log(`${user} joined to ${roomName}.`);
+        })
+        io.to(roomName).emit('joinRoom', roomName, socket.id);
+    })
+
+    socket.on('chatMessage', (message, roomName, user) => {
+        console.log(`${user} send "${message}"`);
+        socket.broadcast.to(roomName).emit('chatMessage', message, socket.id);
+    });
+})
+
+server.listen(PORT, () => {
+    console.log(`server on port ${PORT}!`);
+});
