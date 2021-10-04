234c8ae0a1c69c6a6f9569330e8f25814e978a1c
Yanghyeondong <37038105+Yanghyeondong@users.noreply.github.com>
2021-10-04 14:11:06 +0000

    [update]: Media file serving 설정 추가
    Media file serving 설정 추가를 통해 media 폴더에 파일이 저장되도록 변경
    39

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,6 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'users.User'
 
 SITE_ID = 1
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
