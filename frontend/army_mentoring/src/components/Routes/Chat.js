3b8b7ac9a056d466d15427ed33d4cf8240e5234d
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-10-05 14:25:45 +0000

    [update] : #33 chat 기본 요소 및 스타일 구성

--- frontend/army_mentoring/src/components/Routes/Chat.js
+++ frontend/army_mentoring/src/components/Routes/Chat.js
@@ -1,11 +1,34 @@
 import React, {useState, useEffect } from 'react';
 import './Chat.scss';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
 
 function Chat() {
     
     return (
-        <div>
-            this is chat
+        <div className='chat-body'>
+            <div className='chat-area'>
+                <div className='chat-me'>
+                    <img src="" alt="profile_image" className="profile-image"></img>
+                    <div className="description">
+                        <div className="text">
+                            안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요  
+                        </div>
+                    </div>
+                </div>
+                <div className='chat-you'><div className="description">
+                        <div className="text">
+                            요 안녕하세요  
+                        </div>
+                    </div>
+                    <img src="" alt="profile_image" className="profile-image"></img>
+                </div>
+            </div>
+            <Form>
+                <FormGroup className="typing-area">
+                    <Input className='typing-bar' type="text"></Input>
+                    <Button  className='send-button'>send</Button>
+                </FormGroup>
+            </Form>
         </div>
     );
   }
