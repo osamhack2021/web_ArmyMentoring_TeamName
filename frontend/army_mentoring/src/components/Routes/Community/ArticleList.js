16fa6a59c77ad7ca08484858ccc6c435e7cea94e
nonaninona <53222283+nonaninona@users.noreply.github.com>
2021-10-09 03:53:18 +0000

    [update] : #45 게시글 추가 삭제 기능 연동

--- frontend/army_mentoring/src/components/Routes/Community/ArticleList.js
+++ frontend/army_mentoring/src/components/Routes/Community/ArticleList.js
@@ -5,102 +5,89 @@ import axios from 'axios';
 
 function ArticleList({match}) {
 
-  const [menu,setMenu] = useState([]);
+  const [list,setList] = useState([]);
   //실제로는 sessionStorage에서 가져올 듯?
   const token = '905e125ab3ee40e3a74f6915c9dd3f540b987dc6';
-  axios({
-      method : 'GET',
-      url : 'https://guntor-guntee-data-server.herokuapp.com/auth/user/',
-      headers : { Authorization : '905e125ab3ee40e3a74f6915c9dd3f540b987dc6'}
-  }).then((res)=>{
-      console.log(res);
-  }).catch((err)=>{
-      console.log(err);
-  });
-/*
-  axios({
-      method : 'POST',
-      url : 'https://guntor-guntee-data-server.herokuapp.com/question',
-      header : { Authorization : token },
-      data : {
-          title : '테스트 게시글 1',
-          content : `테스트 내용입니다리 군토군티 화이팅 조기튀김 화이팅
-          좀만 더 힘내요 근데 솔직히 백엔드 연동하는 거 재밌고 신기하다
-          훈련만 없으면 진짜 재밌게 불안하지 않게 할텐데 과연 시간이 충분할지
-          모르겠네용`,
-          user : 'https://guntor-guntee-data-server.herokuapp.com/user/1',
-          liked_user : [] 
-      }
-  }).then((res)=>{
-      console.log(res);
-  }).catch((err)=>{
-      console.log(err);
-  });
-  /* questions 목록 불러오기
-  axios({
-      method : 'GET',
-      url : 'https://guntor-guntee-data-server.herokuapp.com/question'
-  }).then((res)=>{
-      console.log(res);
-  }).catch((err)=>{
-      console.log(err);
-  });
-  */
 
-  useEffect(() => {
-      setMenu([
-        {
-            id:0, title:'What is Lorem Ipsum?',contents:`Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book. It has survived not only five centuries, but also 
-            the leap into electronic typesetting...`
-            ,comments:1,likes:1 
-        },
-        {
-            id:1, title:'What is Lorem Ipsum?',contents:`Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book. It has survived not only five centuries, but also 
-            the leap into electronic typesetting...`
-            ,comments:1,likes:1 
-        },
-        {
-            id:2, title:'What is Lorem Ipsum?',contents:`Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book. It has survived not only five centuries, but also 
-            the leap into electronic typesetting...`
-            ,comments:1,likes:1 
-        },
-        {
-            id:3, title:'What is Lorem Ipsum?',contents:`Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book. It has survived not only five centuries, but also 
-            the leap into electronic typesetting...`
-            ,comments:1,likes:1 
-        }
-    ]);
-    }, []);
+  const load = ()=>{
+        axios({
+            method : 'GET',
+            url : 'https://guntor-guntee-data-server.herokuapp.com/question'
+        }).then((res)=>{
+            console.log(res);
+            setList(res.data);
+        }).catch((err)=>{
+            console.log(err);
+        });
+  }
+  useEffect(()=>{ load() }, [])
+
+  const addButton = ()=>{
+    let id = -1;
+    axios({
+        method : 'GET',
+        url : 'https://guntor-guntee-data-server.herokuapp.com/auth/user',
+        headers : { Authorization : 'Token 905e125ab3ee40e3a74f6915c9dd3f540b987dc6'}
+    }).then((res)=>{
+        console.log(res);
+        id = res.data.id;
+
+        axios({
+            method : 'POST',
+            url : 'https://guntor-guntee-data-server.herokuapp.com/question',
+            headers : { Authorization : 'Token 905e125ab3ee40e3a74f6915c9dd3f540b987dc6'},
+            data : {
+                title : '테스트 게시글 2',
+                content : `테스트 내용입니다리 군토군티 화이팅 조기튀김 화이팅
+                좀만 더 힘내요 근데 솔직히 백엔드 연동하는 거 재밌고 신기하다
+                훈련만 없으면 진짜 재밌게 불안하지 않게 할텐데 과연 시간이 충분할지
+                모르겠네용`,
+                user : 'https://guntor-guntee-data-server.herokuapp.com/user/' + id,
+                liked_user : [ 'https://guntor-guntee-data-server.herokuapp.com/user/' + id ] 
+            }
+        }).then((res)=>{
+            console.log(res);
+            load();
+        }).catch((err)=>{
+            console.log(err.response);
+        });
+    }).catch((err)=>{
+        console.log(err.response);
+    });
+  }
+
+  const removeButton = ()=>{
+    axios({
+        method : 'DELETE',
+        url : 'https://guntor-guntee-data-server.herokuapp.com/question/1',
+        headers : { Authorization : 'Token 905e125ab3ee40e3a74f6915c9dd3f540b987dc6'}
+    }).then((res)=>{
+        console.log(res);
+        load();
+    }).catch((err)=>{
+        console.log(err.response);
+    });
+  }
+
     return (
-    <div>
-        <div className='community_board'>
-        {menu.map((menu) => (
-            <Link to={`${match.url}/${menu.id}`}>
-                <div key={menu.id} className='community_post'>
-                    <div>{"id : " + menu.id}</div>
-                    <div className='community_title'>{menu.title}</div>
-                    <div className='community_contents'>{menu.contents}</div>
-                    <div className='community_statistics'>
-                        <div className='community_comments'>댓글 {menu.comments}</div>
-                        <div className='community_likes'>좋아요 {menu.likes}</div>
+        <div>
+            <div className='community_board'>
+            <div onClick={addButton}>add</div>
+            <div onClick={removeButton}>remove</div>
+            {list.map((li) => (
+                <Link to={`${match.url}/${li.id}`}>
+                    <div key={li.id} className='community_post'>
+                        <div className='community_title'>{li.title}</div>
+                        <div className='community_contents'>{li.content}</div>
+                        <div className='community_statistics'>
+                            <div className='community_comments'>댓글 {li.question_comments.length}</div>
+                            <div className='community_likes'>좋아요 {li.liked_user.length}</div>
+                        </div>
                     </div>
-                </div>
-            </Link>
-        ))}
+                </Link>
+            ))}
+            </div>
         </div>
-    </div>
     );
   }
 
