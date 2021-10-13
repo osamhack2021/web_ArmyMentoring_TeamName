00e8d38dc61aeff7e236f70861d8dc33b7dd50f6
Yanghyeondong <37038105+Yanghyeondong@users.noreply.github.com>
2021-10-13 10:19:44 +0000

    [update]: 홈 화면 css 수정
    홈 화면 css 수정
    gh-58

--- frontend/army_mentoring/src/components/Routes/Home.js
+++ frontend/army_mentoring/src/components/Routes/Home.js
@@ -4,6 +4,7 @@ import './Home.scss';
 import Mentoring_review from './Mentoring_review';
 import 'bootstrap/dist/css/bootstrap.css'
 import soldier from './img/soldier.png';
+import soldier2 from './img/soldier2.png';
 import join from './img/join.png';
 import loupe from './img/loupe.png';
 import support from './img/support.png';
@@ -37,15 +38,19 @@ function Home(){
                         <section className='service_text'>
                             <h2 className="section_title">군대에서의 멘토링 서비스</h2>
                             <h3>누구에게나 의미있고 값진 군생활이 될 수 있도록</h3>
+                            <br/>
+                            <div  className='button_set'>
+                            <Link to="/login/signup"><button>지금 바로 시작하세요</button></Link>
+                            </div>
                         </section>
-                        <img src={soldier}/>
+                        <img src={soldier2}/>
                     </div>
 
-                    <Link to="/mentoring"><button>더 알아보기</button></Link>
+                    
                 </div>
 
                 <div className='mentoring home_section'>
-                    <h2 className="section_title">멘토링 가능한 목록들</h2>
+                    <h2 className="section_title">추천 멘토링 분야</h2>
                     <div className='section_mentoring'>
                         <div className='mentoring_box1 mentoring_img1'>코딩</div>
                         <div className='mentoring_box1 mentoring_img2'>운동</div>
@@ -54,7 +59,11 @@ function Home(){
                         <div className='mentoring_box1 mentoring_img3'>외국어</div>
                         <div className='mentoring_box1 mentoring_img4'>군 생활</div>
                     </div>
-                    <Link to="/mentoring"><button>더 알아보기</button></Link>
+                    <Link to="/mentoring#srchmentoring">
+                        <div className="button_set">
+                            <button className="more">모집중인 멘토링 보기</button>
+                        </div>
+                       </Link>
                  
                 </div>
 
@@ -62,23 +71,23 @@ function Home(){
                     <h2 className="section_title">진행과정</h2>
                     <div className='section_howtodo'>
                         <div className='howtodo_box1'>
-                          <div>1. 회원가입</div>
-                          <div>군토&군티에 회원가입으로 서비스를 시작하세요!</div>
+                          <h3>1. 회원가입</h3>
+                          <div>군토&군티 회원가입으로<br/>서비스를 시작하세요!</div>
                           <img src={join}/>
                         </div>
                     <div className='howtodo_box1'>
-                          <div>2. 멘토링 검색</div>
-                          <div>원하는 분야의 멘토링을 검색하세요!</div>
+                          <h3>2. 멘토링 검색</h3>
+                          <div>원하는 분야의 <br/>멘토링을 검색하세요!</div>
                           <img src={loupe}/>
                     </div>
                     <div className='howtodo_box1'>
-                          <div>3. 멘토링 신청</div>
-                          <div>마음에 드는 멘토링의 소개를 읽어보고 신청하세요!</div>
+                          <h3>3. 멘토링 신청</h3>
+                          <div>마음에 드는 멘토링 소개를<br/>읽어보고 신청하세요!</div>
                           <img src={resume}/>
                     </div>
                     <div className='howtodo_box1'>
-                          <div>4. 멘토링 참여</div>
-                          <div>멘토링 신청이 승낙 되면, 멘토링을 신청하여 시작하세요!</div>
+                          <h3>4. 멘토링 참여</h3>
+                          <div>신청 수락을 받은 후<br/>곧바로 시작하세요!</div>
                           <img src={support}/>
                     </div>
                     </div>
@@ -97,7 +106,7 @@ function Home(){
                             <button onClick={toggling}>Answer!</button>
                         </div>
                         <div className='A'>
-                            <span>ㄴ</span>
+                            
                             <p>
                             군토&군티는 사람마다의 재능의 다양성을 믿으며 멘토/ 멘티 활동에 제한을 두지 않습니다. 배우고, 가르치며 성장해 나가세요!
                             </p>
@@ -109,7 +118,7 @@ function Home(){
                             <button onClick={toggling}>Answer!</button>
                         </div>
                         <div className='A'>
-                            <span>ㄴ</span>
+                            
                             <p>
                             자신있는 분야마다 포트폴리오를 여러가지 생성하여 등록이 가능합니다!
                             </p>
@@ -121,7 +130,7 @@ function Home(){
                             <button onClick={toggling}>Answer!</button>
                         </div>
                         <div className='A'>
-                            <span>ㄴ</span>
+                            
                             <p>
                             멘토링을 진행중인 멘토/멘티와의 협의 후에 중단해주시면 됩니다!
                             </p>
@@ -133,24 +142,13 @@ function Home(){
                             <button onClick={toggling}>Answer!</button>
                         </div>
                         <div className='A'>
-                            <span>ㄴ</span>
+                            
                             <p>
                             진행중인 멘토/멘티와의 협의를 통하여 조정하시면 됩니다! 군토군티의 모든 멘토/멘티들은 모두 국군장병님들이니 부담갖지 않으시고 협의하시면 됩니다
                             </p>
                         </div>
                     </div>
-                    <div className='QnA'>
-                        <div className='Q'>
-                            <p>질문5</p>
-                            <button onClick={toggling}>Answer!</button>
-                        </div>
-                        <div className='A'>
-                            <span>ㄴ</span>
-                            <p>
-                                    대답5
-                            </p>
-                        </div>
-                    </div>
+ 
 
                 </div> 
             </div>
