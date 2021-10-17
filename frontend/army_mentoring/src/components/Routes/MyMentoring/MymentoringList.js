808b58c0ef0df9ddb61988aa5db8bfabb361d882
nagi0101 <62392317+nagi0101@users.noreply.github.com>
2021-10-17 14:38:55 +0000

    [update]: MyMentoringList css 완료
    <description>
    MyMentoringList, MyMentoringCard의 css를 완료하였다.
    추가로 MyMentoringList의 최근 수강 강좌를 클릭했을 때
    해당 멘토링으로 이동하도록 설정하였다.
    
    <issue-number>
    gh-69

--- frontend/army_mentoring/src/components/Routes/MyMentoring/MymentoringList.js
+++ frontend/army_mentoring/src/components/Routes/MyMentoring/MymentoringList.js
@@ -12,7 +12,6 @@ const MENU = [
     {id:'recent', desc:'최근 수강 강좌'},
     {id:'asMentee', desc:'멘티로서 진행중인 멘토링 목록'},
     {id:'asMentor', desc:'멘토로서 진행중인 멘토링 목록'},
-    // {id:'waiting', desc:'신청 승낙 대기중인 멘토링 목록'}
 ]
 
 function MymentoringList({match, history}){  
@@ -36,17 +35,29 @@ function MymentoringList({match, history}){
     const opened_mentoring = user.opened_mentoring || [];
     const recent_mentoring = participated_mentoring[participated_mentoring.length - 1];
 
+    const getMentoringDetailUrlFromApiEndpoint = (apiEndpoint) => {
+        let t = apiEndpoint.split('/');
+        let mid = t[4];
+        return `${match.url}/${mid}`;
+    }
+
     return (
         <div className="MymentoringList">      
             <Subnavbar menu={MENU}></Subnavbar>
 
-            <div className="recent-container section" id="recent">
+            <div className="recent-wrapper section" id="recent">
                 <div className="recent">
                     <h2>최근 수강 강좌</h2>
                     <div className="card-wrapper">
                         {recent_mentoring 
-                        ? <MyMentoringCard mentoringUrl={recent_mentoring} /> 
-                        : <></>
+                        ? (
+                            <Link 
+                            to={getMentoringDetailUrlFromApiEndpoint(recent_mentoring)} 
+                            className="more">
+                                <MyMentoringCard mentoringUrl={recent_mentoring} />
+                            </Link>
+                            ) 
+                        : <p>참여한 멘토링이 없습니다!</p>
                         }
                     </div>
                 </div>
@@ -57,13 +68,14 @@ function MymentoringList({match, history}){
                 <div className="mentoring_cards">
                 {
                     participated_mentoring.map((element, index) => {
-                        let t = element.split('/');
-                        let mid = t[4];
                         return (
-                            <Link to={`${match.url}/${mid}`} key={index} className="more">
+                            <Link 
+                            to={getMentoringDetailUrlFromApiEndpoint(element)} 
+                            key={index} 
+                            className="more">
                                 <MyMentoringCard mentoringUrl={element}  />
                             </Link>
-                            )
+                        )
                     })
                 }
                 </div>
@@ -86,10 +98,6 @@ function MymentoringList({match, history}){
                 }
                 </div>
             </div>
-
-            {/* <div className="section" id="waiting">
-                <h2>신청 승낙 대기중인 멘토링 목록</h2>
-            </div> */}
         </div>
     )
 
